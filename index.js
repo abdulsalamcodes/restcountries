3c636b23fa1787817439329e24a617be61ba4322
abdulsalamcodes <59220264+abdulsalamcodes@users.noreply.github.com>
2021-01-16 15:20:50 +0100

    Completed Project - v1

--- index.js
+++ index.js
@@ -1,99 +1,163 @@
-let countries = document.querySelector('.names');
-let search = document.querySelector('.search');
+let countries = document.querySelector('.countries');
+let select_region = document.querySelector('#region ');
 let input = document.querySelector('#input');
 let api = "https://restcountries.eu/rest/v2/all";
-let acountry = document.createElement('div');
+let modal = document.querySelector(".modal");
+let modalWrapper = document.querySelector(".modal__wrapper");
+let back = document.querySelector(".back");
+let borderWrapper = document.querySelector(".border__names");
 
-console.log(acountry);
-let output = function (element) {
-    
-    let content =  `
-    <div class="item">
-        <div>
-            <img src=${element.flag} >
-        </div>
-        <div class= "item__content">
-            <p><strong>Name:</strong> ${element.name}</p>
-            <p><strong>Capital:</strong> ${element.capital}</p>
-            <p><strong>Population:</strong> ${element.population}</p>
-        <div>
-    </div>
-        `        
-    acountry.append(content);
-    console.log(acountry)
-    return content;
+// Helper Function
+const showModal = (item) => {
+    createModalContent(item)
+    modal.classList.add("showModal")
+}
 
+// Close Modal
+back.addEventListener("click", () => {
+    modal.classList.remove("showModal");
+})
+
+
+// Handle Country listing
+const populateData = (data) => {
+    data.map(item => {
+        // Reusable Component
+        let title = (text) => document.createElement("strong").textContent = text
+
+        let countryBox = document.createElement("div");
+        countryBox.classList.add("country__box");
+
+        let image = document.createElement("img");
+        image.src = item.flag;
+        countryBox.appendChild(image);
+
+        let countryName = document.createElement('h3');
+        countryName.textContent = item.name;
+        countryName.className = "country__name";
+        countryBox.appendChild(countryName);
+
+
+        let countryDetail = document.createElement("div");
+        countryDetail.classList.add("country__detail");
+        countryBox.appendChild(countryDetail);
+
+        let population = document.createElement("p");
+        population.textContent = title("Population") + ": " + item.population;
+        countryDetail.appendChild(population);
+
+        let Region = document.createElement("p");
+        Region.textContent = title("Region") + ": " + item.region;
+        countryDetail.appendChild(Region);
+
+        let Capital = document.createElement("p");
+        Capital.textContent = title("Capital") + ": " + item.capital;
+        countryDetail.appendChild(Capital);
+
+        countryBox.addEventListener("click", () => showModal(item))
+        countries.appendChild(countryBox)
+    })
+}
+
+// Handle Modal Content
+const createModalContent = (item) => {
+    let image = document.querySelector(".modal__image");
+    image.src = item.flag;
+
+    let title = document.querySelector("#title");
+    title.textContent = item.name;
+
+    let nativeName = document.querySelector("#native_name");
+    nativeName.textContent = item.nativeName;
+
+    let population = document.querySelector("#population");
+    population.textContent = item.population;
+    let sub_region = document.querySelector("#sub_region");
+    sub_region.textContent = item.subregion;
+    let capital = document.querySelector("#capital");
+    capital.textContent = item.capital;
+    let top_level_domain = document.querySelector("#top_level_domain");
+    top_level_domain.textContent = item.topLevelDomain;
+
+    let currenciesCodes = item.currencies.map(currency => currency.code);
+    document.querySelector("#currencies").textContent = currenciesCodes.join(",")
+
+    let languagesName = item.languages.map(language => language.name);
+    document.querySelector("#languages").textContent = languagesName.join(',');
+
+    // clear border first
+    borderWrapper.innerHTML = "";
+
+    item.borders.map(border => {
+
+        let borderButton = document.createElement("button");
+        borderButton.addEventListener("click", () => handleBorderModal(border))
+        borderButton.textContent = border;
+        borderWrapper.appendChild(borderButton);
+    })
+
+    // return wrapper;
 }
 
 
-function findCountry(element) {
-    if (element.name == input.value) {
-        console.log(element);
-        countries.innerHTML = output(element)
-    }
-}
-// function getData() {
-
-    fetch(api)
-    .then(status)
-        .then(response => {
-            data = response.json();
-            return data;
-        })
-    
+const handleBorderModal = (code) => {
+    fetch(`https://restcountries.eu/rest/v2/alpha/${code}`)
+        .then(response => response.json())
         .then(data => {
-            
-            input.addEventListener('input', () => {
-                
-                data.forEach(element => {
-                    let regex = new RegExp('^' + input.value, 'i');
-                    let name = element.name;
-                    
-                    if (regex.test(name)) {
-                        countries.insertAdjacentHTML('afterbegin', output(element))
-                     }// else {
-                         //     // countries.insertAdjacentHTML('afterbegin', output1(element)) 
-                         //     output(element).remove()
-                         // }
-                        })
-                    })
-                    
-                    search.addEventListener('click', (event) => {
-                        event.preventDefault()
-                        data.forEach(element => { 
-                    findCountry(element) 
-                });
-            });
-            
-            data.forEach(element => {
-                countries.insertAdjacentHTML('afterbegin', output(element))
-            });
-    
+            borderWrapper.innerHTML = "";
+            showModal(data);
         })
-    
-        .catch(error => {
-            console.log(error);
+}
+
+let ThemeSwitcher = document.querySelector("#theme__switcher");
+
+ThemeSwitcher.addEventListener("click", () => {
+    if (document.documentElement.getAttribute('data-theme') !== "dark") {
+        document.documentElement.setAttribute('data-theme', "dark");
+    } else {
+        document.documentElement.setAttribute('data-theme', "light");
+    }
+})
+
+fetch(api)
+    .then(response => {
+        return response.json();
+    })
+    .then(data => {
+        // Initialize
+        populateData(data);
+
+        // Filter Based on Input
+        input.addEventListener("input", (e) => {
+            var newData = data.filter(item => {
+                return (item.name.toLowerCase().includes(e.target.value)) ? item : null
+            })
+            countries.innerHTML = "";
+            populateData(newData)
         })
-// }
-
-// getData()
 
 
+        // Filter based on selected Option.
+        select_region.addEventListener("change", e => {
+            console.log(e.target.value)
+            if (e.target.value == "all") {
+                console.log("hey")
+                populateData(data);
+                return;
+            };
+            let newData = [...data].filter(item => {
+                return (item.region.toLowerCase() === e.target.value.toLowerCase() ? item : null)
+            })
+
+            countries.innerHTML = "";
+            populateData(newData)
+        })
 
 
+    })
+
+    .catch(error => {
+        console.log(error);
+    })
 
 
-// api = `https://restcountries.eu/rest/v2/name/${regex.test(name)}`;
-
-// fetch(api)
-//         .then(status)
-//         .then(response => {
-//             data = response.json();
-//             return data;
-//         })
-
-//         .then(data => {
-//             data.forEach(element => {
-//                 countries.insertAdjacentHTML('afterbegin', output(element))
-//             });
-//     })
